bc52864f7e3ef2df01b375e670db993c0d92d3d4
YashodaraR <110415793+YashodaraR@users.noreply.github.com>
2022-10-16 01:12:27 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,20 @@
+
+//  window.onload = loadCoupon();
+
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility='visible';
+    document.getElementById('main').style.opacity='0.6';
+    
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility='hidden';
+    document.getElementById('main').style.opacity = '1';
+}
+
+            // const checkbox = document.getElementById('checkbox');
+            // checkbox.addEventListener('change',() =>{
+            //     document.body.classList.toggle('dark');
+
+
+            // })
